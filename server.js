896774d7efb500ec17a563f063d4744f2a931727
Zeeshan Paracha <zparacha82@gmail.com>
2021-06-22 21:37:35 +0500

    db config

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
